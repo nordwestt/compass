d1da5c7e2a7e270834cd9d3710dd0e27428b82bd
Thomas Nordentoft <thomas@nordentoft.dk>
2024-11-22 19:43:33 +0100

    Collapsible sidebar

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,15 +1,12 @@
-import { Image, StyleSheet, Platform, View } from 'react-native';
+import { Image, StyleSheet, Platform, View, Text, TouchableOpacity } from 'react-native';
 
-import { HelloWave } from '@/components/HelloWave';
-import ParallaxScrollView from '@/components/ParallaxScrollView';
-import { ThemedText } from '@/components/ThemedText';
-import { ThemedView } from '@/components/ThemedView';
 import { ChatThread } from '@/components/ChatThread';
 import { ChatMessage, SelectedModel } from '@/hooks/useChat';
 import { ChatThreads } from '@/components/ChatThreads';
 import { useSignal } from '@preact/signals-react';
 import { SystemPrompt } from '@/components/SystemPromptSelector';
 import { useSignals } from '@preact/signals-react/runtime';
+import Ionicons from '@expo/vector-icons/Ionicons';
 
 export interface Thread {
   id: string;
@@ -23,11 +20,25 @@ export default function HomeScreen() {
   useSignals();
   const threads = useSignal<Thread[]>([{id: Date.now().toString(), title: "First conversation", messages:[], selectedModel: {id: '', provider: {type: 'ollama', endpoint: '', apiKey: ''}}, systemPrompt: {id: 'default', name: 'Default Assistant', content: 'You are a helpful AI assistant.'}}]);
   const currentThread = useSignal<Thread>(threads.value[0]);
+  const isSidebarVisible = useSignal(true);
   return (
     <View className="bg-gray-100 flex-1 flex-row">
-      <View className="w-64">
-        <ChatThreads threads={threads} currentThread={currentThread}></ChatThreads>
-      </View>
+      {isSidebarVisible.value && (
+        <View className="w-64 border-r-2 border-gray-200 dark:border-gray-700">
+          <View className="flex-row justify-between items-center p-4 bg-gray-200 dark:bg-gray-700">
+            <Text className="text-center text-lg font-bold text-black dark:text-white">Threads</Text>
+          <TouchableOpacity onPress={() => {isSidebarVisible.value = !isSidebarVisible.value}} className="p-2 rounded-full bg-gray-200 dark:bg-gray-700">
+            <Ionicons name={"chevron-back"} size={24} className="text-black dark:text-white" />
+          </TouchableOpacity>
+        </View>
+          <ChatThreads threads={threads} currentThread={currentThread}></ChatThreads>
+        </View>
+      )}
+      {!isSidebarVisible.value && (
+        <TouchableOpacity onPress={() => {isSidebarVisible.value = !isSidebarVisible.value}} className="absolute left-2 top-1/2 -translate-y-1/2 z-10 p-2 rounded-full bg-gray-200 dark:bg-gray-700">
+          <Ionicons name={"chevron-forward"} size={24} className="text-black dark:text-white" />
+        </TouchableOpacity>
+      )}
       <View className="flex-1 bg-white rounded-t-xl">
         <ChatThread thread={currentThread} threads={threads}/>
       </View>
