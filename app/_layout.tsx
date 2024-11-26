3ba92f47969b925ac2b0814b3aadb2688def9c95
Thomas Nordentoft <thomas@nordentoft.dk>
2024-11-26 02:36:58 +0100

    Also theme header bar

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -7,10 +7,12 @@ import 'react-native-reanimated';
 import { ThemeProvider } from '@/components/ThemeProvider';
 import { ConfirmationModal } from '@/components/ConfirmationModal';
 import { colorScheme } from 'nativewind';
+import { useTheme } from "@/hooks/useTheme";
 
 SplashScreen.preventAutoHideAsync();
 
 export default function RootLayout() {
+  const { theme } = useTheme();
   const [loaded] = useFonts({
     SpaceMono: require('../assets/fonts/SpaceMono-Regular.ttf'),
     'Caveat-Regular': require('../assets/fonts/Caveat-Regular.ttf'),
@@ -30,7 +32,14 @@ export default function RootLayout() {
 
   return (
     <ThemeProvider>
-      <Stack>
+      <Stack screenOptions={{
+            headerStyle: {
+              backgroundColor: theme.colors.surface,
+            },
+            headerTintColor: theme.colors.text,
+            headerShadowVisible: false,
+            headerBackTitleVisible: false,
+          }}>
         <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
         <Stack.Screen name="+not-found" />
       </Stack>
