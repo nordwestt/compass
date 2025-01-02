7ad3f981c5e82a4fefe6323123b9a2f3a95b0ef0
Thomas Nordentoft <thomas@nordentoft.dk>
2025-01-02 18:06:10 +0100

    Create dropdown

---
+++ components/Dropdown.tsx
@@ -0,0 +1,59 @@
+import { View, Text, TouchableOpacity, Image, ScrollView } from "react-native"
+import React, { useState } from 'react';
+
+export interface DropdownElement {
+    title: string;
+    id: string;
+    image: string;
+}
+
+interface DropdownProps {
+    children: DropdownElement[];
+    selected: DropdownElement | null;
+    onSelect: (child: DropdownElement) => void;
+}
+
+export const Dropdown = ({ children, selected, onSelect }: DropdownProps) => {
+
+    const [isOpen, setIsOpen] = useState(false);
+    
+  return ( 
+  <View>
+    <TouchableOpacity 
+        onPress={() => setIsOpen(!isOpen)}
+        className="flex-row items-center px-2 h-12 py-2 rounded-lg bg-background hover:bg-surface border border-border"
+      >
+        {selected?.image && (
+          <Image source={selected.image as any} className="!h-[48px] !w-[48px] rounded-full mr-3"/>
+        )}
+        <Text className="font-medium text-black dark:text-white">
+          {selected?.title}
+        </Text>
+      </TouchableOpacity>
+      {isOpen && <View className="absolute z-200 mt-12 rounded-lg overflow-hidden w-64 max-h-64 bg-background border border-border shadow-lg">
+        <ScrollView>
+        {children.map((child) => (
+          <TouchableOpacity
+            key={child.id}
+            onPress={() => {
+              onSelect(child);
+              setIsOpen(false);
+            }}
+            className={`w-64 flex-row items-center p-3 hover:bg-surface`}
+          >
+            {child.image && (
+              <Image source={child.image as any} className={`!h-[48px] !w-[48px] rounded-full mr-3  ${selected?.id === child.id ? "border-primary border-4" : ""}`}/>
+            )}
+            <View className="flex-1">
+              <Text className="font-medium text-black dark:text-white">
+                {child.title}
+              </Text>
+            </View>
+          </TouchableOpacity>
+        ))}
+        </ScrollView>
+      </View>}
+    </View>
+  )
+  
+}
--- components/ModelSelector.tsx
+++ components/ModelSelector.tsx
@@ -7,6 +7,7 @@ import { availableProvidersAtom, availableModelsAtom, defaultModelAtom } from '@
 import { PROVIDER_LOGOS } from '@/src/constants/logos';
 import { ThemeProvider } from './ThemeProvider';
 import { Provider } from '@/types/core';
+import { DropdownElement } from './Dropdown';
 
 import Animated, { 
   FadeIn,
@@ -17,6 +18,7 @@ import Animated, {
 import { fetchAvailableModelsV2 } from '@/hooks/useModels';
 import { scanForOllamaInstances } from '@/src/components/providers/providers';
 import { toastService } from '@/services/toastService';
+import { Dropdown } from './Dropdown';
 
 
 
@@ -29,10 +31,10 @@ export const ModelSelector: React.FC<ModelSelectorProps> = ({
   selectedModel,
   onSetModel,
 }) => {
-  const [isModalVisible, setIsModalVisible] = useState(false);
   const [providers, setProviders] = useAtom(availableProvidersAtom);
   const [models, setModels] = useAtom(availableModelsAtom);
   const [defaultModel, setDefaultModel] = useAtom(defaultModelAtom);
+  const [dropdownModel, setDropdownModel] = useState<DropdownElement | null>(null);
 
   React.useEffect(() => {
     const fetchModels = async () => {
@@ -74,6 +76,14 @@ export const ModelSelector: React.FC<ModelSelectorProps> = ({
     fetchModels();
   }, []);
 
+  function setDropdownModell(model: Model) {
+    setDropdownModel({
+      title: model.name,
+      id: model.id,
+      image: PROVIDER_LOGOS[model.provider.source as keyof typeof PROVIDER_LOGOS]
+    });
+  }
+
   // Add useEffect to handle initial model selection
   React.useEffect(() => {
     if (!models.length) return;
@@ -86,6 +96,7 @@ export const ModelSelector: React.FC<ModelSelectorProps> = ({
         onSetModel(models[0]);
       }
     }
+    setDropdownModell(selectedModel);
   }, [models, selectedModel.id, defaultModel, onSetModel]);
 
 
@@ -98,107 +109,20 @@ export const ModelSelector: React.FC<ModelSelectorProps> = ({
 
   // Remove the model selection logic from render phase
   const currentModel = models.find(m => m.id === selectedModel.id);
+  
+
+  let modelList = models.map((model) => ({
+    title: model.name,
+    id: model.id,
+    image: PROVIDER_LOGOS[model.provider.source as keyof typeof PROVIDER_LOGOS]
+  }));
+
+  //modelList = [...modelList, ...(modelList.map(x=>({...x, id: "wuut"})))];
+
 
   return (
     <>
-      <TouchableOpacity 
-        onPress={() => setIsModalVisible(true)}
-        className="flex-row items-center px-2 h-12 py-2 rounded-lg bg-background hover:bg-surface border border-border"
-      >
-        {PROVIDER_LOGOS[selectedModel.provider.source as keyof typeof PROVIDER_LOGOS] && (
-          <Image 
-            source={PROVIDER_LOGOS[selectedModel.provider.source as keyof typeof PROVIDER_LOGOS]}
-            className="!h-[32px] !w-[32px] rounded-full"
-          />
-        )}
-        {Platform.OS == 'web' && (
-          <Text className="ml-2 text-black dark:text-white">
-            {currentModel ? `${currentModel.name}` : 'Select Model'}
-          </Text>
-        )}
-      </TouchableOpacity>
-
-      <Modal
-        visible={isModalVisible}
-        transparent={true}
-        animationType="none"
-        onRequestClose={() => setIsModalVisible(false)}
-      >
-        <ThemeProvider>
-          <Animated.View 
-            entering={FadeIn.duration(200)}
-            exiting={FadeOut.duration(200)}
-            className="absolute inset-0 bg-black/50"
-          />
-          <View className="flex-1 justify-end">
-            <Animated.View 
-              entering={SlideInDown.springify().damping(15)}
-              exiting={SlideOutDown.duration(200)}
-              className="rounded-t-xl max-h-[70%] bg-background"
-            >
-              <View className="p-4 border-b border-gray-200 dark:border-gray-700">
-                <Text className="text-lg font-bold text-center text-black dark:text-white">
-                  Select Model
-                </Text>
-              </View>
-              
-              <ScrollView className="p-4">
-                {models.map((model) => (
-                  <TouchableOpacity
-                    key={model.id}
-                    onPress={() => {
-                      onSetModel(model);
-                      //setIsModalVisible(false);
-                    }}
-                    className="flex-row items-center p-3 mb-2 rounded-lg bg-surface"
-                  >
-                    {PROVIDER_LOGOS[model.provider.source as keyof typeof PROVIDER_LOGOS] && (
-                      <Image
-                        source={PROVIDER_LOGOS[model.provider.source as keyof typeof PROVIDER_LOGOS]}
-                        className="!h-[48px] !w-[48px] rounded-full mr-3"
-                      />
-                    )}
-                    <View className="flex-1">
-                      <Text className="font-medium text-black dark:text-white">
-                        {model.name}
-                      </Text>
-                      <Text className="text-sm text-gray-500 dark:text-gray-400">
-                        {model.provider.name}
-                      </Text>
-                    </View>
-                    {model.id === selectedModel.id && (
-                      <View className="bg-primary px-2 py-1 rounded">
-                        <Text className="text-white text-sm">Selected</Text>
-                      </View>
-                    )}
-                  </TouchableOpacity>
-                ))}
-              </ScrollView>
-
-              <View className="p-4 border-t border-gray-200 dark:border-gray-700 flex-row justify-between">
-                <TouchableOpacity
-                  onPress={() => setIsModalVisible(false)}
-                  className="flex-1 mr-2 bg-surface p-2 rounded-lg">
-                  <Text className="text-center text-text">
-                    Cancel
-                  </Text>
-                </TouchableOpacity>
-                <TouchableOpacity
-                  onPress={() => {
-                    setDefaultModel(selectedModel);
-                    setIsModalVisible(false);
-                  }}
-                  className="flex-1 ml-2 bg-primary p-2 rounded-lg"
-                >
-                  <Text className="text-center text-white font-medium">
-                    Set as Default
-                  </Text>
-                </TouchableOpacity>
-              </View>
-            </Animated.View>
-          </View>
-        </ThemeProvider>
-      </Modal>
+      <Dropdown selected={dropdownModel} onSelect={setDropdownModel} children={modelList} />
     </>
   );
 }; 
