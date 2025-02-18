5f5ff9dccbc389dba9a84fa7ddd2621487a58964
Thomas Nordentoft <thomas@nordentoft.dk>
2025-02-18 23:03:47 +0100

    Fixes

--- src/components/character/CharacterAvatar.tsx
+++ src/components/character/CharacterAvatar.tsx
@@ -18,13 +18,13 @@ export const CharacterAvatar: React.FC<CharacterAvatarProps> = ({
     return (
       <Image 
         source={character.image} 
-        className={`!h-[64px] !w-[64px] rounded-full ${className}`}
+        className={`${size == 64?'!h-[64px] !w-[64px]': size == 32?'!h-[32px] !w-[32px]': ''} rounded-full ${className}`}
       />
     );
   }
 
   return (
-    <View className={`!h-[64px] !w-[64px] rounded-full bg-primary items-center justify-center ${className}`} style={{ aspectRatio: 1 }}>
+    <View className={`${size == 64?'!h-[64px] !w-[64px]': size == 32?'!h-[32px] !w-[32px]': ''} rounded-full bg-primary items-center justify-center ${className}`} style={{ aspectRatio: 1 }}>
       <Ionicons 
         name={character.icon || 'person' as any} 
         size={size * 0.6} 
