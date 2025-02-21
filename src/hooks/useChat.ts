9e078cb93a1463dfbcf171ac8c5c2ce7e3910374
Thomas Nordentoft <thomas@nordentoft.dk>
2025-02-21 23:38:04 +0100

    tidy

--- src/hooks/useChat.ts
+++ src/hooks/useChat.ts
@@ -7,16 +7,9 @@ import { CharacterContextManager } from '@/src/services/chat/CharacterContextMan
 import { StreamHandlerService } from '@/src/services/chat/StreamHandlerService';
 import { ChatProviderFactory } from '@/src/services/chat/ChatProviderFactory';
 import { useSearch } from './useSearch';
-import { current } from 'tailwindcss/colors';
 import { Character, ChatMessage, Thread } from '@/src/types/core';
 import LogService from '@/utils/LogService';
 import { toastService } from '@/src/services/toastService';
-//import TurndownService from 'turndown';
-import { Readability } from '@mozilla/readability';
-//import { JSDOM } from 'jsdom';
-import { getProxyUrl } from '../utils/proxy';
-import { searchRelevantPassages } from '../utils/semanticSearch';
-import { fetchSiteText } from '../utils/siteFetcher';
 import { MessageContext, MessageTransformPipeline, relevantPassagesTransform, urlContentTransform, searchTransform, threadUpdateTransform, firstMessageTransform } from './pipelines';
 
 export function useChat() {
@@ -38,28 +31,6 @@ export function useChat() {
     tts.stopStreaming();
   };
 
-  const handleFirstMessage = async (
-    message: string,
-    thread: Thread
-  ): Promise<void> => {
-    const systemPrompt = `Based on the user message, generate a concise 3-word title that captures the essence of the conversation. Format: "Word1 Word2 Word3" (no quotes, no periods but do include spaces).`;
-
-    const provider = ChatProviderFactory.getProvider(thread.selectedModel);
-    try {
-      const title = await provider.sendSimpleMessage(message, thread.selectedModel, systemPrompt);
-      await new Promise(resolve => setTimeout(resolve, 200));
-
-      dispatchThread({
-        type: 'update',
-        payload: { ...thread, title: title }
-      });
-    } catch (error: any) {
-      toastService.danger({title: 'Error generating title', description: "The AI service may be experiencing issues. Please try again later."});
-      LogService.log(error, { component: 'useChat', function: 'handleFirstMessage' }, 'error');
-    }
-  }
-
-  
 
   const pipeline = new MessageTransformPipeline()
     .addTransform(urlContentTransform)
