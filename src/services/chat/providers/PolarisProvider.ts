cc1198a37f4afa9dfabe2b2ba15b6fbf3c9c414c
Thomas Nordentoft <thomas@nordentoft.dk>
2025-03-31 17:52:10 +0200

    Working with polaris

--- src/services/chat/providers/PolarisProvider.ts
+++ src/services/chat/providers/PolarisProvider.ts
@@ -1,116 +1,157 @@
-import { ChatProvider } from '@/src/types/chat';
-import { Character, Provider } from '@/src/types/core';
-import { ChatMessage } from '@/src/types/core';
-import { Model } from '@/src/types/core';
-import LogService from '@/utils/LogService';
-import { toastService } from '@/src/services/toastService';
-import { CoreMessage, embedMany, generateText, streamText, tool } from 'ai';
-import { createOllama } from 'ollama-ai-provider';
-import { fetch as expoFetch } from 'expo/fetch';
-import { z } from 'zod';
-import { getProxyUrl } from '@/src/utils/proxy';
-import { embed } from 'ai';
+import { ChatProvider } from "@/src/types/chat";
+import { Character, Provider } from "@/src/types/core";
+import { ChatMessage } from "@/src/types/core";
+import { Model } from "@/src/types/core";
+import LogService from "@/utils/LogService";
+import { toastService } from "@/src/services/toastService";
+import { CoreMessage, embedMany, generateText, streamText, tool } from "ai";
+import { createOllama } from "ollama-ai-provider";
+import { fetch as expoFetch } from "expo/fetch";
+import { z } from "zod";
+import { getProxyUrl } from "@/src/utils/proxy";
+import { embed } from "ai";
 
-
-
-import {Platform as PlatformCust} from '@/src/utils/platform';
-import {streamOllamaResponse} from '@/src/services/chat/streamUtils';
-
+import { Platform as PlatformCust } from "@/src/utils/platform";
+import { streamPolarisResponse } from "@/src/services/chat/streamUtils";
 
 export class PolarisProvider implements ChatProvider {
   provider: Provider;
   constructor(provider: Provider) {
     this.provider = provider;
   }
-  async *sendMessage(messages: ChatMessage[], model: Model, character: Character, signal?: AbortSignal): AsyncGenerator<string> {
+  async *sendMessage(
+    messages: ChatMessage[],
+    model: Model,
+    character: Character,
+    signal?: AbortSignal,
+  ): AsyncGenerator<string> {
     const newMessages = [
-      { role: 'system', content: character.content },
-      ...messages.map(message => ({ 
-        role: message.isUser ? 'user' : message.isSystem ? 'system' : 'assistant', 
-        content: message.content 
-      }))
+      { role: "system", content: character.content },
+      ...messages.map((message) => ({
+        role: message.isUser
+          ? "user"
+          : message.isSystem
+            ? "system"
+            : "assistant",
+        content: message.content,
+      })),
     ];
 
     // if latest message is empty
-    if(newMessages[newMessages.length-1].content.trim() === ''){
+    if (newMessages[newMessages.length - 1].content.trim() === "") {
       newMessages.pop();
     }
 
-    try{
-        let url = `${model.provider.endpoint}/api/chat/stream`;
-        if(PlatformCust.isTauri) url = await getProxyUrl(url);
-        yield* streamOllamaResponse(url, {
+    try {
+      let url = `${model.provider.endpoint}/api/chat/stream`;
+      if (PlatformCust.isTauri) url = await getProxyUrl(url);
+      console.log("Sending request to", url);
+      // let response = await fetch(url, {
+      //   method: 'POST',
+      //   headers: { 'Content-Type': 'application/json', 'Authorization': `Bearer ${this.provider.apiKey}` },
+      //   body: JSON.stringify({
+      //     model: model.id,
+      //     messages: newMessages,
+      //     stream: false
+      //   })
+      // });
+      // let data = await response.json();
+      // return data.message.content;
+      yield* streamPolarisResponse(
+        url,
+        {
           model: model.id,
-          messages: newMessages
-        }, {
-          'Authorization': `Bearer ${this.provider.apiKey}`
-        });
-    }
-    catch(error:any){
-      LogService.log(error, {component: 'OllamaProvider', function: `sendMessage: ${model.provider.endpoint}`}, 'error');
+          messages: newMessages,
+          stream: true,
+        },
+        {
+          Authorization: `Bearer ${this.provider.apiKey}`,
+        },
+      );
+    } catch (error: any) {
+      LogService.log(
+        error,
+        {
+          component: "OllamaProvider",
+          function: `sendMessage: ${model.provider.endpoint}`,
+        },
+        "error",
+      );
       throw error;
     }
   }
 
-
-  async sendSimpleMessage(message: string, model: Model, systemPrompt: string): Promise<string> {
+  async sendSimpleMessage(
+    message: string,
+    model: Model,
+    systemPrompt: string,
+  ): Promise<string> {
     return "Test message";
   }
 
-  
-  async sendJSONMessage(message: string, model: Model, systemPrompt: string): Promise<any>{
+  async sendJSONMessage(
+    message: string,
+    model: Model,
+    systemPrompt: string,
+  ): Promise<any> {
     let url = `${model.provider.endpoint}/api/chat`;
     let response = await fetch(await getProxyUrl(url), {
-        method: 'POST',
-        headers: { 'Content-Type': 'application/json' },
-        body: JSON.stringify({
-          model: model.id,
-          messages: [{ role: 'system', content: systemPrompt }, { role: 'user', content: message }],
-          stream: false,
-          format: 'json'
-        }),
-      });
-      let data = await response.json();
-    
-    try{
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({
+        model: model.id,
+        messages: [
+          { role: "system", content: systemPrompt },
+          { role: "user", content: message },
+        ],
+        stream: false,
+        format: "json",
+      }),
+    });
+    let data = await response.json();
+
+    try {
       return JSON.parse(data.message.content);
     } catch (error) {
-      return {query: "", searchRequired: false};
+      return { query: "", searchRequired: false };
     }
   }
 
   async embedText(texts: string[]): Promise<number[][]> {
     let url = `${this.provider.endpoint}/api/embed`;
-    if(PlatformCust.isTauri) url = await getProxyUrl(url);
+    if (PlatformCust.isTauri) url = await getProxyUrl(url);
 
     const ollama = createOllama({
       // optional settings, e.g.
-      baseURL: await getProxyUrl(this.provider.endpoint+'/api'),
+      baseURL: await getProxyUrl(this.provider.endpoint + "/api"),
       fetch: expoFetch as unknown as typeof globalThis.fetch,
     });
-    
+
     const { embeddings } = await embedMany({
-      model: ollama.embedding('all-minilm'),
+      model: ollama.embedding("all-minilm"),
       values: texts,
     });
 
     return embeddings;
-
   }
 
   async getAvailableModels(): Promise<string[]> {
-
-    const response = await fetch(await getProxyUrl(`${this.provider.endpoint}/api/chat/characters`), {
-      headers: {
-        'Accept': 'application/json',
-        'Authorization': `Bearer ${this.provider.apiKey}`
-      }
-    });
+    const response = await fetch(
+      await getProxyUrl(`${this.provider.endpoint}/api/chat/characters`),
+      {
+        headers: {
+          Accept: "application/json",
+          Authorization: `Bearer ${this.provider.apiKey}`,
+        },
+      },
+    );
     const data = await response.json();
-    
+
     if (!(data && Array.isArray(data))) return [];
     return data
-      .filter((character: any) => character && typeof character.name === 'string')
+      .filter(
+        (character: any) => character && typeof character.name === "string",
+      )
       .map((character: any) => character.name);
   }
-} 
+}
--- src/services/chat/streamUtils.ts
+++ src/services/chat/streamUtils.ts
@@ -1,6 +1,6 @@
-import { Model } from '@/src/types/core';
+import { Model } from "@/src/types/core";
 
-export interface streamOptions{
+export interface streamOptions {
   headers?: Record<string, string>;
   signal?: AbortSignal;
   parseChunk?: (parsed: any) => string;
@@ -8,36 +8,36 @@ export interface streamOptions{
 export async function* streamOpenAIResponse(
   url: string,
   payload: any,
-  options?: streamOptions
+  options?: streamOptions,
 ): AsyncGenerator<string> {
-  let parseChunk = (parsed: any) => parsed?.choices[0]?.delta?.content || '';
-  if(options?.parseChunk) {
+  let parseChunk = (parsed: any) => parsed?.choices[0]?.delta?.content || "";
+  if (options?.parseChunk) {
     parseChunk = options.parseChunk;
   }
-  let headers = { 'Content-Type': 'application/json' };
-  if(options?.headers) {
+  let headers = { "Content-Type": "application/json" };
+  if (options?.headers) {
     headers = {
       ...headers,
-      ...options.headers
-    }
+      ...options.headers,
+    };
   }
-    
+
   console.log("streaming response", url);
-  const response =  await fetch(url, {
-  method: 'POST',
-  headers,
-  signal: options?.signal,
-  body: JSON.stringify({
+  const response = await fetch(url, {
+    method: "POST",
+    headers,
+    signal: options?.signal,
+    body: JSON.stringify({
       ...payload,
-      stream: true
-  })
+      stream: true,
+    }),
   } as any);
   const reader = response.body?.getReader();
   if (!reader) {
-    throw new Error('No reader available');
+    throw new Error("No reader available");
   }
 
-  let buffer = '';
+  let buffer = "";
   const decoder = new TextDecoder();
 
   let error = false;
@@ -53,18 +53,17 @@ export async function* streamOpenAIResponse(
     for (const chunk of chunks) {
       try {
         let bob = chunk.split("data: ")[1];
-        if(!bob) {
+        if (!bob) {
           continue;
         }
         const parsedChunk = JSON.parse(bob);
         yield parseChunk(parsedChunk);
-        buffer = '';
+        buffer = "";
       } catch {
         console.log("failed", chunk);
         continue;
       }
     }
-
   }
 }
 
@@ -72,29 +71,30 @@ export async function* streamOllamaResponse(
   url: string,
   payload: any,
   headers?: Record<string, string>,
-  parseChunk: (parsed: any) => string = (parsed) => parsed.message?.content || '',signal?: AbortSignal
+  parseChunk: (parsed: any) => string = (parsed) =>
+    parsed.message?.content || "",
+  signal?: AbortSignal,
 ): AsyncGenerator<string> {
-    
-  const response =  await fetch(url, {
-  method: 'POST',
-  headers: { 'Content-Type': 'application/json', ...headers },
-  signal,
-  body: JSON.stringify({
+  const response = await fetch(url, {
+    method: "POST",
+    headers: { "Content-Type": "application/json", ...headers },
+    signal,
+    body: JSON.stringify({
       ...payload,
-      stream: true
-  })
+      stream: true,
+    }),
   } as any);
   const reader = response.body?.getReader();
   if (!reader) {
-    throw new Error('No reader available');
+    throw new Error("No reader available");
   }
 
-  let buffer = '';
+  let buffer = "";
   const decoder = new TextDecoder();
 
   while (true) {
     const { done, value } = await reader.read();
-    console.log("chunk", done, value);  
+    console.log("chunk", done, value);
     if (done) break;
 
     const chunk = decoder.decode(value);
@@ -105,10 +105,45 @@ export async function* streamOllamaResponse(
       try {
         const parsedChunk = JSON.parse(chunk);
         yield parseChunk(parsedChunk);
-        buffer = '';
+        buffer = "";
       } catch {
         continue;
       }
+    }
+  }
+}
+
+export async function* streamPolarisResponse(
+  url: string,
+  payload: any,
+  headers?: Record<string, string>,
+  parseChunk: (parsed: any) => string = (parsed) =>
+    parsed.message?.content || "",
+  signal?: AbortSignal,
+): AsyncGenerator<string> {
+  const response = await fetch(url, {
+    method: "POST",
+    headers: { "Content-Type": "application/json", ...headers },
+    signal,
+    body: JSON.stringify({
+      ...payload,
+      stream: true,
+    }),
+  } as any);
+  const reader = response.body?.getReader();
+  if (!reader) {
+    throw new Error("No reader available");
+  }
+
+  while (true) {
+    const { value, done } = await reader.read();
+    if (done) break;
+
+    // Convert the received chunks to text
+    const chunk = new TextDecoder().decode(value);
+    let lines = chunk.split("\n");
+    for (let line of lines) {
+      if (line.startsWith("0:")) yield line.substring(3, line.length - 1);
     }
   }
 }
